4bd56dd0edc23905fa70c3c79be8d1d8ca7096dd
Will Pursell <pursell.will@gmail.com>
2022-01-05 21:26:55 -0500

    Removed Data

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth"
 
 const app = firebase.initializeApp({
-    apiKey: 'AIzaSyCX71QqA0ar65GUQnS8-FICaW56z04ZoZ4',
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
