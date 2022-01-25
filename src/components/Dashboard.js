afe9eb557eac61fd942b52550ae07d64662227a9
Will <pursell.will@gmail.com>
2022-01-24 19:08:40 -0500

    Added APOD data to logged in users dashboard (Not styled)

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import { Row, Col, Container, Card } from 'react-bootstrap'
+import { useState, useEffect } from 'react';
 
-export default function Dashboard() {
+const apodApiKey = process.env.REACT_APP_APOD_API_KEY;
+
+export default function NasaPhoto() {
+    const [photoData, setPhotoData] = useState(null);
+
+    useEffect(() => {
+        fetchPhoto();
+
+        async function fetchPhoto() {
+            const res = await fetch(
+                `https://api.nasa.gov/planetary/apod?api_key=${apodApiKey}` 
+            )
+
+            const data = await res.json();
+            setPhotoData(data);
+        }
+    }, []);
+
+    if (!photoData) return <div />;
+
     return (
         <>
             <Container>
@@ -12,26 +32,25 @@ export default function Dashboard() {
                     <Col>
                         <Card>
                             <Card.Body>
-                                <h2 className='text-center mb-2'>Card</h2>
+                                <img src={photoData.url} alt={photoData.title} width="100%" className="photo" />
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col>
                         <Card>
                             <Card.Body>
-                                <h2 className='text-center mb-2'>Card</h2>
+                                <p className="date">{photoData.date}</p>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col>
                         <Card>
                             <Card.Body>
-                                <h2 className='text-center mb-2'>Card</h2>
+                                <p className="explanation">{photoData.explanation}</p>
                             </Card.Body>
                         </Card>
                     </Col>
                 </Row>
             </Container>
         </>
-    )
-}
+    )}
